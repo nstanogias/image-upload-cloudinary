4c36a5c06f4a08ac7912ea22f57716b4e5884571
Nikos Stanogias <nstanogias@gmail.com>
2018-07-29 00:00:41 +0200

    invalidate credentials

--- config/credentials_dev.js
+++ config/credentials_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  secretAccessKey: 'jGnJfF0HY4VSh8+g3hZ6QFx7IJvvKpmDpnxpgSn5',
-  accessKeyId: 'AKIAJ4HDTDNXGBWGL55Q',
-  region: 'us-east-1'
+  secretAccessKey: '',
+  accessKeyId: '',
+  region: ''
 };
